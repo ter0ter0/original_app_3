b730faf4565614cc6d8f333668110a16994d9570
takeuchi <panicdepanic@gmail.com>
2025-05-29 08:01:30 +0900

    コンフリクト解消

--- src/laravelapp/resources/views/posts/posts.blade.php
+++ src/laravelapp/resources/views/posts/posts.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
     @if(Auth::check())
+        <div class="text-center mt-3"> 
+            <p class="fw-bold">ビジョンについて語り合おう</p>
+        </div>
         <div class="d-flex align-items-center justify-content-center mt-3" >
             <form method="POST" action="{{ route('post.store') }}" class="d-inline-block w-75" enctype="multipart/form-data">
                 @csrf
@@ -14,10 +17,10 @@
 
     @foreach($posts as $post)
     <div class="d-flex align-items-center justify-content-center pb-3" >
-        <div class="card d-flex align-items-center justify-content-center" style="width: 50rem;">
+        <div class="card" style="width: 50rem;">
             <div class="card-body">
-                <p class="mb-2">{{$post->user->name}}</p>
-                <p class="mb-2">{!! nl2br(e($post->content)) !!}</p>
+                <p class="text-start fw-bold">{{$post->user->name}}</p>
+                <p class="text-center">{!! nl2br(e($post->content)) !!}</p>
             </div>
         </div>
     </div>
